d58f661b81b078402d1f2b250dd1f2b568c6330c
Zakwan Ashfaq Zian <zakwanashfaq@gmail.com>
2024-04-22 23:58:35 -0230

    layout templates added

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -1,20 +1,33 @@
+'use client';
+
+import Footer from "src/components/footer"
+import Navbar from "src/components/navbar"
+import '../scss/custom.scss';
+import logo from '../assets/icnl-logo-no-bg.jpg'
+
 export default function RootLayout({
-    children,
-  }: {
-    children: React.ReactNode
-  }) {
-    return (
-      <html lang="en">
-        <head>
-          <meta charSet="UTF-8" />
-          <link rel="icon" href="%PUBLIC_URL%/favicon.ico" />
-          <meta name="viewport" content="width=device-width, initial-scale=1" />
-          <title>ICNL</title>
-          <meta name="description" content="Islamic Center of Newfoundland" />
-        </head>
-        <body>
-          <div id="root">{children}</div>
-        </body>
-      </html>
-    )
-  }
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <head>
+        <meta charSet="UTF-8" />
+        <link rel="icon" href={logo.src} />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <title>ICNL</title>
+        <meta name="description" content="Islamic Center of Newfoundland" />
+      </head>
+      <body>
+        <div className='p-0 m-0' style={{ minHeight: "100vh", display: "flex", flexDirection: "column" }}>
+          <Navbar />
+          <div className='container-lg flex-grow-1'>
+           <div id="root">{children}</div>
+          </div>
+          <Footer />
+        </div>
+      </body>
+    </html>
+  )
+}
--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,12 +1,52 @@
 'use client';
 
-import HomePage from "src/page_templates/homePage";
+import { useEffect, useState } from "react";
+import AboutUsWidget from "src/components/aboutUsWidget";
+import PrayerBannerTimetable from "src/components/prayerBannerTimetable";
 
-
-export default function Page  () {
+function LoadingPrayerTimeTable() {
+    const LoadingText = "Loading prayer times"
     return (
         <>
-            <HomePage />
+            <div className=''>
+                <div className='rounded-4 placeholder bg-primary text-light fw-bold d-flex jutify-content-center align-items-center flex-column p-5'>
+                    {LoadingText}
+                    <div className="d-flex mt-3 justify-content-center">
+                        <div className="spinner-border" role="status">
+                            <span className="visually-hidden">Loading</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+}
+
+export default function Page() {
+    const [landingPageData, setLandingPageData] = useState(null as any);
+    useEffect(() => {
+        fetch('https://k80pnjbrb8.execute-api.us-east-1.amazonaws.com/Prod/landingPageData')
+            .then(response => response.json())  // Parse the response as JSON
+            .then(data => {
+                console.log(data);
+                setLandingPageData(data.Item);
+            })
+            .catch(error => console.error('An error occurred:\n', error));
+    }, []);
+
+    return (
+        <>
+            {landingPageData ? <PrayerBannerTimetable prayerTimes={landingPageData.prayerTimes} /> : <LoadingPrayerTimeTable />}
+            <br />
+            <div className="row pb-4">
+            {/* <div className="col-12 col-lg-4">
+                <UpcomingEventsWidget />
+            </div> */}
+            {/* <div className="col-12">
+                <ActivitiesWidget />
+            </div> */}
+            <AboutUsWidget />
+            </div>
         </>
     );
 };
