2921b550145ba36376a7e8248f39571f47d152d9
Zakwan Ashfaq Zian <zakwanashfaq@gmail.com>
2024-04-23 13:25:15 -0230

    init cypress

--- server/dotnet_api/Program.cs
+++ server/dotnet_api/Program.cs
@@ -1,7 +1,7 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddRazorPages();
+
 
 var app = builder.Build();
 
@@ -20,6 +20,6 @@ app.UseRouting();
 
 app.UseAuthorization();
 
-app.MapRazorPages();
+app.MapControllers();
 
 app.Run();
