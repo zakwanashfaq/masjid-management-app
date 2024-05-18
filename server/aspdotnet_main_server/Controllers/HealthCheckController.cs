3f5ea3f1798931032884ea9c4e6d00e285ab4f1e
Zakwan Ashfaq Zian <zakwanashfaq@gmail.com>
2024-05-18 11:57:15 -0230

    health check added and docker compose configured

--- server/aspdotnet_main_server/Controllers/HealthCheckController.cs
+++ server/aspdotnet_main_server/Controllers/HealthCheckController.cs
@@ -3,20 +3,20 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 
-namespace aspdotnet_main_server.Controllers
+namespace aspdotnet_main_server.Controllers;
+
+[ApiController]
+[Route("healthCheck/")]
+public class HealthCheckController : ControllerBase
 {
-    [ApiController]
-    [Route("healthCheck/")]
-    public class HealthCheckController : ControllerBase
+
+    [HttpGet]
+    [Route("/health")]
+    public ActionResult GetHealthCheckStatus()
     {
-
-        [HttpGet]
-        [Route("/")]
-        public ActionResult GetHealthCheckStatus()
-        {
-            return Ok(new { message = "Server is healthy"});
-        }
-
-        
+        return Ok(new { message = "Server is healthy"});
     }
+
+    
 }
+
